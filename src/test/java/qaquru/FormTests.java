edd418f9c47bb2fee8d622870a4e26cb907583d6
Ksenia <kseniarassadina@gmail.com>
2021-07-05 20:27:16 +0300

    Added lasts steps for test

--- src/test/java/qaquru/FormTests.java
+++ src/test/java/qaquru/FormTests.java
@@ -4,10 +4,13 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
+import static com.codeborne.selenide.Condition.selectedText;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.files.DownloadActions.click;
 
 
 public class FormTests {
@@ -25,10 +28,26 @@ public class FormTests {
         $("#userEmail").setValue("alexandra@gmail.com");
         $(byText("Female")).click();
         $("#userNumber").setValue("9999990000");
-
-
-
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").click();
+        $(".react-datepicker__month-select").selectOption("November");
+        $(".react-datepicker__year-select").click();
+        $(".react-datepicker__year-select").selectOption("1995");
+        $(".react-datepicker__day--001:nth-child(4)").click();
+        $("#subjectsInput").setValue("M").pressEnter();
+        $("#uploadPicture").uploadFromClasspath(("xh7m8nokzhgfqixb00fnwcmkthm.jpeg"));
+        $(byText("Reading")).click();
         $("#currentAddress").setValue("some home on some street");
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("Haryana")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Panipat")).click();
+        $("#submit").click();
+
+
+
+
+
 
     }
 }
